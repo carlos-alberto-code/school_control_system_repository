3f9838d27b4cbabf8f4567df716b7d3e8994b00c
Carlos Baltazar <A840159943@my.uvm.edu.mx>
2025-01-04 15:16:21 -0600

    Refactor: actualizar importación de configuración de conexión en main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+from conn import config
 from school_control_system_repository.repository import Repository
-from school_control_system_repository.config import DatabaseConfig
 from school_control_system_repository.models import Student, Teacher
 
 
-with DatabaseConfig.get_session() as session:
+with config.get_session() as session:
     repository = Repository[Student](model=Student, session=session)
     repository.get(Student.id == 1)
